e2789aa2418320861a86ac8bd464a596a248ce0a
kholostyak17 <kholostyak17@gmail.com>
2021-04-29 14:34:57 +0000

    cloned my p14 repository

---
+++ src/js/component/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SecondsCounter from "./secondscounter.jsx";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+export function Home() {
+	return <SecondsCounter />;
+}
